e7978259a719699164e435eb604f70b30a6f345b
keerthanreddy691 <keerthansankepally@gmail.com>
2026-05-19 14:56:06 +0530

    Implement employee deletion functionality
    Added functionality to delete an employee and navigate after deletion.

--- mern-app-emps-master/mern-app-emps-master/frontend/src/components/DeleteEmp.jsx
+++ mern-app-emps-master/mern-app-emps-master/frontend/src/components/DeleteEmp.jsx
@@ -1,14 +1,35 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function DeleteEmp() {
   const { state } = useLocation();
+  const navigate = useNavigate();
 
-  console.log(state); // selected employee object
+  const deleteEmployee = async () => {
+    const res = await fetch(
+      `https://emp-backend-zdvn.onrender.com/emp-api/employees/${state._id}`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    const data = await res.json();
+    alert(data.message);
+
+    navigate("/");
+  };
 
   return (
-    <div>
+    <div className="text-center">
       <h1>Delete Employee</h1>
+
       <p>{state.name}</p>
+
+      <button
+        onClick={deleteEmployee}
+        className="bg-red-600 text-white p-2 rounded"
+      >
+        Confirm Delete
+      </button>
     </div>
   );
 }
